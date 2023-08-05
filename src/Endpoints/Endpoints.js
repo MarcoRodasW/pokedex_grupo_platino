ba5d536d51dcf2174dec3aef2e3b58249dbbd1f5
Marco Rodas <mrodasw@gmail.com>
2023-08-05 13:33:04 -0600

    Added new endpoints

--- src/Endpoints/Endpoints.js
+++ src/Endpoints/Endpoints.js
@@ -1,3 +1,5 @@
 export const API_TYPE = 'https://pokeapi.co/api/v2/type/'
 export const API_ALLPOKEMONS = 'https://pokeapi.co/api/v2/pokemon?limit=20'
 export const API_GETPOKEMON = 'https://pokeapi.co/api/v2/pokemon'
+export const API_GETEVOLUTIONS = 'https://pokeapi.co/api/v2/evolution-chain'
+export const API_GETSPECIES = 'https://pokeapi.co/api/v2/pokemon-species'
