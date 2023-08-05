98c7816b37c547e5363ffede0430dbbeecd6cecc
Marco Rodas <mrodasw@gmail.com>
2023-08-05 13:34:07 -0600

    Handle filter functionality

--- src/components/FilterSection/SearchInput.jsx
+++ src/components/FilterSection/SearchInput.jsx
@@ -1,15 +1,36 @@
-const SearchInput = () => {
+import React, { useState } from 'react' // Don't forget to import React
+
+const SearchInput = ({ onSearch }) => {
+  const [inputValue, setInputValue] = useState('')
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    onSearch(inputValue)
+  }
+
   return (
-    <>
-      <div className=" flex justify-center mb-5">
-        <form action="" className="flex flex-row justify-evenly items-center space-x-5 w-full max-w-lg">
-          <input type="text" placeholder="Search Pokemon" className="input input-bordered w-full max-w-lg" />
-          <button type="submit" className="btn btn-primary">
-            Search
-          </button>
-        </form>
-      </div>
-    </>
+    <div className="flex justify-center mb-5">
+      <form
+        action=""
+        className="flex flex-row justify-evenly items-center space-x-5 w-full max-w-lg"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          placeholder="Search Pokemon"
+          className="input input-bordered w-full max-w-lg"
+          value={inputValue}
+          onChange={handleChange}
+        />
+        <button type="submit" className="btn btn-primary">
+          Search
+        </button>
+      </form>
+    </div>
   )
 }
 
--- src/components/PokemonSection/Pokemons.jsx
+++ src/components/PokemonSection/Pokemons.jsx
@@ -1,24 +1,35 @@
 import { useState, useEffect } from 'react'
 import PokemonCard from './PokemonCard'
 import Pagination from './Pagination'
-import { API_ALLPOKEMONS } from '../../Endpoints/Endpoints'
+import { API_ALLPOKEMONS, API_GETPOKEMON } from '../../Endpoints/Endpoints'
+import { FilterSection } from '../FilterSection/FilterSection'
+import SearchInput from '../FilterSection/SearchInput'
+import { Categories } from '../FilterSection/Categories'
 
 export const Pokemons = () => {
   const [pokemons, setPokemons] = useState([])
   const [nextUrl, setNextUrl] = useState(null)
   const [prevUrl, setPrevUrl] = useState(null)
+  const [searchQuery, setSearchQuery] = useState('')
+  console.log(searchQuery)
 
   useEffect(() => {
     async function fetchData() {
       const res = await fetch(API_ALLPOKEMONS)
       const data = await res.json()
-      setPokemons(data.results)
+
+      // Filter Pokemons based on the search query
+      const filteredPokemons = data.results.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(searchQuery.toLowerCase()),
+      )
+
+      setPokemons(filteredPokemons)
       setNextUrl(data.next)
       setPrevUrl(data.previous)
     }
 
     fetchData()
-  }, [])
+  }, [searchQuery])
 
   const goToNextPage = async () => {
     const res = await fetch(nextUrl)
@@ -37,13 +48,17 @@ export const Pokemons = () => {
   }
 
   return (
-    <section className="m-10">
-      <div className="grid grid-cols-3 gap-4">
-        {pokemons.map((pokemon) => (
-          <PokemonCard key={pokemon.name} pokemonUrl={pokemon.url} />
-        ))}
-      </div>
-      <Pagination goToPrevPage={goToPrevPage} goToNextPage={goToNextPage} />
-    </section>
+    <>
+      <SearchInput onSearch={setSearchQuery} />
+      <Categories />
+      <section className="m-10">
+        <div className="grid grid-cols-3 gap-4">
+          {pokemons.map((pokemon) => (
+            <PokemonCard key={pokemon.name} pokemonUrl={pokemon.url} />
+          ))}
+        </div>
+        {<Pagination goToPrevPage={goToPrevPage} goToNextPage={goToNextPage} />}
+      </section>
+    </>
   )
 }
