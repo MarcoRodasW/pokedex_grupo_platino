30cba45a4af1fe9bb12289b06e3e325ba44fbf0d
Marco Rodas <mrodasw@gmail.com>
2023-08-06 18:19:49 -0600

    Dark theme by default configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['dark']
+  },
+  plugins: [require('daisyui')]
 }
