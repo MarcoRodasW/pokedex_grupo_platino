95087f6f9a9d7bab7cddcfefa99690e53021692c
Marco Rodas <mrodasw@gmail.com>
2023-08-06 18:20:17 -0600

    Added responsive to main element

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import Header from './components/Header'
-import { FilterSection } from './components/FilterSection/FilterSection'
 import { Pokemons } from './components/PokemonSection/Pokemons'
 
 function App() {
   return (
-    <main className="container mx-auto w-screen h-screen">
+    <main className="container md:container md:mx-auto sm:container sm:mx-auto mx-auto lg:w-screen md:h-screen">
       <Header />
       <Pokemons />
     </main>
