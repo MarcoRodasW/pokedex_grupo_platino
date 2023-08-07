0319263341154b6e582f012637fc307921217f61
Marco Rodas <mrodasw@gmail.com>
2023-08-06 18:28:47 -0600

    Responsive classes added

--- src/components/PokemonSection/PokemonCard.jsx
+++ src/components/PokemonSection/PokemonCard.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'wouter'
 import usePokemonData from '../../Hooks/usePokemonData'
+import PokemonTypes from './PokemonTypes'
 function PokemonCard({ pokemonUrl }) {
   const { pokemonsData, loading } = usePokemonData(pokemonUrl)
 
@@ -10,22 +11,18 @@ function PokemonCard({ pokemonUrl }) {
   const { id, name, sprites, types } = pokemonsData
 
   return (
-    <div className="card w-96 h-70 p-5 bg-base-100 shadow-xl flex flex-col">
+    <div className="card w-96 h-70 p-5 bg-base-100 shadow-xl flex flex-col items-center justify-center transition duration-300 ease-in-out hover:scale-105">
       <Link href={`/pokemon/${name}`}>
         <a className="cursor-pointer">
           <figure>
             <img src={sprites.front_default} alt={name} className="object-contain w-40 h-40" />
           </figure>
-          <div className="card-body">
+          <div className="card-body flex flex-col items-center justify-center">
             <h2 className="card-title capitalize">{name}</h2>
             <p>Number of Pokedex {id}</p>
           </div>
           <div className="card-actions justify-center">
-            {types.map(({ type: { name } }, slot) => (
-              <button className="btn btn-neutral no-animation" key={slot}>
-                {name}
-              </button>
-            ))}
+            <PokemonTypes types={types} />
           </div>
         </a>
       </Link>
--- src/components/PokemonSection/PokemonEvolutions.jsx
+++ src/components/PokemonSection/PokemonEvolutions.jsx
@@ -6,9 +6,9 @@ export default function PokemonEvolutions({ pokemonId }) {
   const evolvesToData = useEvolutions(pokemonId)
 
   return (
-    <div className="flex gap-4">
+    <>
       {evolvesToData &&
         evolvesToData.map(({ name }, index) => <PokemonCard key={index} pokemonUrl={`${API_GETPOKEMON}/${name}`} />)}
-    </div>
+    </>
   )
 }
